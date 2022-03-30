5e36e20d836eedc6a98451930065165034629d17
Rushi <189489rishi@gmail.com>
2022-03-30 18:01:51 +0530

    adding gender

--- PayRoll.sql
+++ PayRoll.sql
@@ -1,9 +1,8 @@
-
---UC-1 create database
+--UC1 - Create Payroll_Service Database.
 create database PayRoll_Service;
 use PayRoll_Service;
 
---UC2 create employee payroll table
+--UC2 - Create employee_payroll table.
 create table employee_payroll
 (
   id int primary key not null identity(1,1),
@@ -11,19 +10,24 @@ create table employee_payroll
   salary float not null,
   startDate date
 )
-select * from employee_payroll
 
---UC3 - inserting values to table
+--UC3 - inserting values to table.
 insert into employee_payroll (nameofStudent, Salary, StartDate) values
-('rushi', 30000.00, '22-Mar-2022'),
-('pratham', 80000.00, '17-June-2007'),
-('vikrant', 40000.00, '18-May-2010'),
-('prashant', 70000.00, '09-Jan-2009');
+('Bill', 100000.00, '2018-01-03'),
+('Terisa', 200000.00, '2019-11-13'),
+('Charlie', 300000.00, '2020-05-21');
 
---UC4 - Retrieve employee_payroll data
+--UC4 - Retrieve employee_payroll data.
 select * from employee_payroll
 
---UC5 - Retrieve salary of emp and  particular date range.
+--UC5 - Retrieve salary of particular employee and in particular date range.
 select salary from employee_payroll where nameofStudent = 'Bill';
 select * from employee_payroll
 where StartDate between CAST ('2018-01-01' as date) and GETDATE()
+
+--UC6 - add Gender to Employee_Payroll Table and Update the Rows to reflect the correct Employee Gender.
+Alter table employee_payroll add Gender char(1);
+
+Update employee_payroll set Gender='F' where nameofStudent = 'Terisa';
+Update employee_payroll set Gender='M' where nameofStudent = 'Bill' or nameofStudent = 'Charlie';
+update employee_payroll set salary = 300000.00 where nameofStudent = 'Terisa';
