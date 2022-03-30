ea7b95260e4b53740f72abff741e92c2338ed3da
Rushi <189489rishi@gmail.com>
2022-03-30 12:23:34 +0530

    adding table

--- PayRoll.sql
+++ PayRoll.sql
@@ -1,2 +1,14 @@
+
+--UC-1 create database
 create database PayRoll_Service;
 use PayRoll_Service;
+
+--UC2 create employee payroll table
+create table employee_payroll
+(
+  id int primary key not null identity(1,1),
+  nameofStudent varchar (150) not null,
+  salary float not null,
+  startDate date
+)
+select * from employee_payroll
